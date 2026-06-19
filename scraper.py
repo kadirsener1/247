3e154fe0c8e2dd7b3fc9a03e2405bf96e3efa93b
kadirsener1 <119889025+kadirsener1@users.noreply.github.com>
2026-06-19 09:31:54 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -11,7 +11,7 @@ from datetime import datetime
 # ─────────────────────────────────────────────
 # YAPILANDIRMA
 # ─────────────────────────────────────────────
-BASE_URL = "https://tv247.biz/watch/"
+BASE_URL = "https://tvnow.club/watch/"
 OUTPUT_FILE = "tv247.m3u"
 CHANNELS_FILE = "channels.txt"
 
